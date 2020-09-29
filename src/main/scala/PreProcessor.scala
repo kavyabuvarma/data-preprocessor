2dfc41daec75cc8918ae361e535cf07a59c70cbe
Kavya Varma <kvarma@shelde.com>
2020-09-29 14:41:41 +1000

    data anonymization

---
+++ src/main/scala/PreProcessor.scala
@@ -0,0 +1,30 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object PreProcessor{
+  def main(args: Array[String]) {
+
+    var logFile = "data_1mb.csv"
+
+    if (args.length > 0 ){
+      logFile = args(0)
+    }
+
+    val spark = SparkSession.builder.appName("Preprocessor").getOrCreate()
+
+    val df_original = spark.read.option("header", true).csv(logFile)
+    df_original.printSchema()
+    df_original.show(2, false)
+
+    val df_anonymized = df_original.select(col("date_of_birth"),
+      lit("XXXXXX").as("first_name"),
+      lit("YYYYYY").as("last_name"),
+      regexp_replace(df_original.col("address"), "(^[A-Z]+ [A-Z]+)",  "ZZZZZZZZZZ").as("address"),
+      )
+
+    df_anonymized.printSchema()
+    df_anonymized.show(2, false)
+
+    spark.stop()
+  }
+}
